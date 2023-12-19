f9d1a93ef9db1c3747c69cf50ba901572673095c
Arman Pestaño <arman.paulo.me@gmail.com>
2023-12-19 21:53:38 +0800

    Space - LT(_numfunc, KC_SPC)

--- keyboards/lily58/keymaps/arman639/keymap.c
+++ keyboards/lily58/keymaps/arman639/keymap.c
@@ -74,14 +74,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                           KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS, KC_LSFT, 
   KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCLN, RSFT_T(KC_QUOT), 
   KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BTN2,                 KC_BTN1, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT, 
-                    KC_LALT, KC_LGUI, TD(ALT_OSL1), KC_SPC,      KC_ENT, TD(ALT_OSL2), KC_RALT, KC_RCTL),
+                    KC_LALT, KC_LGUI, TD(ALT_OSL1), LT(_NUMFUNC, KC_SPC),      KC_ENT, TD(ALT_OSL2), KC_RALT, KC_RCTL),
 
 [_LAYER5] = LAYOUT(
   KC_NO, KC_NO, LCTL(KC_PSCR), KC_PSCR, KC_NO, KC_NO,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
   LGUI(KC_DOWN), LGUI(KC_1), LGUI(KC_2), LGUI(KC_3), LGUI(KC_4), LGUI(KC_5),          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
   KC_ESC, LCTL(KC_HOME), OS_ALT, OS_SHFT, OS_CTRL, KC_NO,                               KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
   KC_NO, LCTL(KC_END), KC_INS, KC_CAPS, LSFT(KC_F10), KC_NO, KC_NO,                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
-                                        KC_NO, KC_NO, KC_NO, KC_NO,                   KC_TRNS, KC_NO, KC_TRNS, KC_TRNS),
+                                        KC_NO, KC_NO, KC_NO, KC_TRNS,                   KC_TRNS, KC_NO, KC_TRNS, KC_TRNS),
 
 [_HOMERIGHT] = LAYOUT(
   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, QK_BOOT, 
@@ -102,7 +102,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TAB, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                    KC_NO, KC_HOME, KC_UP, KC_END, KC_DEL, KC_NO, 
   KC_LSFT, KC_NO, KC_LALT, KC_LSFT, KC_LCTL, KC_NO,                             KC_PGUP, KC_LEFT, KC_DOWN, KC_RGHT, KC_BSPC, KC_WH_U, 
   KC_LCTL, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), KC_NO, KC_NO,        KC_SCRL, KC_PGDN, LCTL(KC_PGUP), KC_NO, LCTL(KC_PGDN), RGUI(KC_T), KC_WH_D, 
-                              KC_TRNS, KC_TRNS, MO(3), KC_SPC,                  KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS),
+                              KC_TRNS, KC_TRNS, MO(3), KC_TRNS,                  KC_ENT, KC_TRNS, KC_TRNS, KC_TRNS),
 
 
 [_NUMFUNC] = LAYOUT(
@@ -110,7 +110,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_LALT, KC_F6, KC_F7, KC_F8, KC_F9, LCA(KC_PAUS),                                  KC_F9, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PAUS, 
   KC_LSFT, KC_WH_U, KC_F10, KC_F11, KC_F12, KC_PGUP,                                  KC_NO, KC_P4, KC_P5, KC_P6, KC_BSPC, KC_ESC, 
   KC_LCTL, KC_WH_D, LALT(KC_LEFT), LALT(KC_RGHT), LALT(KC_F5), KC_PGDN, KC_TRNS,      KC_TRNS, KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_RCTL, 
-                    KC_TRNS, KC_TRNS, KC_NO, KC_SPC,                                  KC_ENT, KC_P0, KC_PDOT, KC_RALT),
+                    KC_TRNS, KC_TRNS, KC_NO, KC_TRNS,                                  KC_ENT, KC_P0, KC_PDOT, KC_RALT),
 
 
 [_LAYER6] = LAYOUT(
