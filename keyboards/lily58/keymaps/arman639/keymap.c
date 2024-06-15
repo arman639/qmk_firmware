ab96757fc72bae1a2b01237763be8ccda4d225fd
Arman Pestaño <arman.paulo.me@gmail.com>
2024-06-15 23:40:49 +0800

    new keymap

--- keyboards/lily58/keymaps/arman639/keymap.c
+++ keyboards/lily58/keymaps/arman639/keymap.c
@@ -109,7 +109,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                         KC_NO, KC_PSLS, KC_PAST, KC_PMNS, KC_NO, KC_NUM, 
   KC_LALT, KC_F6, KC_F7, KC_F8, KC_F9, LCA(KC_PAUS),                                  KC_F9, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PAUS, 
   KC_LSFT, KC_WH_U, KC_F10, KC_F11, KC_F12, KC_PGUP,                                  KC_NO, KC_P4, KC_P5, KC_P6, KC_BSPC, KC_ESC, 
-  KC_LCTL, KC_WH_D, LALT(KC_LEFT), LALT(KC_RGHT), LALT(KC_F5), KC_PGDN, KC_TRNS,      KC_TRNS, KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_RCTL, 
+  KC_LCTL, KC_WH_D, LALT(KC_LEFT), LALT(KC_RGHT), LALT(KC_F5), KC_PGDN, LGUI(KC_PGUP),      KC_TRNS, KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_RCTL, 
                     KC_TRNS, KC_TRNS, KC_NO, KC_TRNS,                                  KC_ENT, KC_P0, KC_PDOT, KC_RALT),
 
 
