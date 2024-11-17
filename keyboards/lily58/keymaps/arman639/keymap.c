f0dfe4384bd4820573abe7815dae5cb68066e25c
arman639 <arman.paulo.me@gmail.com>
2024-11-17 15:19:19 +0800

    remapping mouse layer

--- keyboards/lily58/keymaps/arman639/keymap.c
+++ keyboards/lily58/keymaps/arman639/keymap.c
@@ -11,7 +11,7 @@ extern uint8_t is_master;
 #define _LAYER5 5
 #define _LAYER6 6
 #define _LAYER7 7
-#define _LAYER8 8
+#define _MOUSE 8
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -72,7 +72,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_BASE] = LAYOUT(
   KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5,                           KC_6, KC_7, KC_8, KC_9, KC_0, KC_GRV,
   KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T,                           KC_Y, KC_U, KC_I, KC_O, KC_P, KC_MINS,
-  KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCLN, RSFT_T(KC_QUOT),
+  LM(_MOUSE, MOD_LSFT), KC_A, KC_S, KC_D, KC_F, KC_G,           KC_H, KC_J, KC_K, KC_L, KC_SCLN, RSFT_T(KC_QUOT),
   KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BTN2,                 KC_BTN1, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
                     KC_LALT, KC_LGUI, TD(ALT_OSL1), LT(_NUMFUNC, KC_SPC),      OSM(MOD_RSFT), TD(ALT_OSL2), KC_RALT, KC_RCTL),
 
@@ -127,10 +127,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                  KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_TRNS,
                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
-[_LAYER8] = LAYOUT(
+[_MOUSE] = LAYOUT(
   KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                         KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,
-  KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                         KC_NO, KC_BTN1, KC_MS_U, KC_BTN2, KC_NO, KC_NO,
-  KC_TRNS, KC_ACL0, KC_BTN2, KC_BTN3, KC_BTN1, KC_NO,                                         KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_U, KC_NO,
+  KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                         KC_NO, KC_NO, KC_MS_U, KC_NO, KC_NO, KC_NO,
+  KC_LSFT, KC_ACL0, KC_BTN2, KC_BTN3, KC_BTN1, KC_NO,                                         KC_BTN1, KC_MS_L, KC_MS_D, KC_MS_R, KC_WH_U, KC_NO,
   KC_TRNS, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NO,                                  KC_NO, KC_NO, KC_WH_L, KC_NO, KC_WH_R, KC_WH_D, KC_TRNS,
                   KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS)
 };
@@ -210,7 +210,7 @@ void alt2_finished (qk_tap_dance_state_t *state, void *user_data) {
   switch (alttap_state.state) {
     case SINGLE_TAP: layer_on(_HOMERIGHT); is_oneshot_active = true; break;
     case SINGLE_HOLD: layer_on(_NAV); break;
-    case DOUBLE_HOLD: layer_on(_LAYER8); break;
+    case DOUBLE_HOLD: layer_on(_MOUSE); break;
   }
 }
 
@@ -219,7 +219,7 @@ void alt2_reset (qk_tap_dance_state_t *state, void *user_data) {
     case SINGLE_TAP: break;
     case SINGLE_HOLD: layer_off(_NAV); break;
     case DOUBLE_TAP: break;
-    case DOUBLE_HOLD: layer_off(_LAYER8); break;
+    case DOUBLE_HOLD: layer_off(_MOUSE); break;
 	  case TRIPLE_HOLD: break;
   }
   alttap_state.state = 0;
