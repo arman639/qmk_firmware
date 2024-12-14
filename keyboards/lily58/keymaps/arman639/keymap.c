2766745420763bdcdd50bb694e04ae0c96bd1eca
arman639 <arman.paulo.me@gmail.com>
2024-12-14 15:59:20 +0800

    remapped function keys

--- keyboards/lily58/keymaps/arman639/keymap.c
+++ keyboards/lily58/keymaps/arman639/keymap.c
@@ -105,11 +105,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                               KC_TRNS, KC_TRNS, KC_TRNS, KC_SPC,                      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS),
 
 [_NUMFUNC] = LAYOUT(
-  KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                         KC_NO, KC_PSLS, KC_PAST, KC_PMNS, KC_NO, KC_NUM,
-  KC_LALT, KC_F6, KC_F7, KC_F8, KC_F9, LCA(KC_PAUS),                                  KC_F9, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PAUS,
-  KC_LSFT, KC_WH_U, KC_F10, KC_F11, KC_F12, KC_PGUP,                                  KC_NO, KC_P4, KC_P5, KC_P6, KC_BSPC, KC_ESC,
-  KC_LCTL, KC_WH_D, LALT(KC_LEFT), LALT(KC_RGHT), LALT(KC_F5), KC_PGDN, LGUI(KC_PGUP),      KC_TRNS, KC_NO, KC_P1, KC_P2, KC_P3, KC_PEQL, KC_RCTL,
-                    KC_TRNS, KC_TRNS, KC_NO, KC_TRNS,                                  KC_ENT, KC_P0, KC_PDOT, KC_RALT),
+  KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NUM,
+  KC_LALT, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7,                                           KC_PAST, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PAUS,
+  KC_LSFT, KC_F2, KC_F10, KC_F11, KC_F12, KC_F8,                                        KC_F9, KC_P4, KC_P5, KC_P6, KC_BSPC, KC_ESC,
+  KC_LCTL, KC_F1, LALT(KC_LEFT), LALT(KC_RGHT), LALT(KC_F5), KC_F9, LGUI(KC_PGUP),   KC_TRNS, KC_PMNS, KC_P1, KC_P2, KC_P3, KC_PSLS, KC_RCTL,
+                    KC_TRNS, KC_TRNS, KC_NO, KC_TRNS,                                   KC_ENT, KC_P0, KC_PDOT, KC_RALT),
 
 [_OSL_T_1] = LAYOUT(
   KC_NO, KC_NO, LCTL(KC_PSCR), KC_PSCR, KC_NO, KC_NO,                                 KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
@@ -232,7 +232,7 @@ void alt_reset (tap_dance_state_t *state, void *user_data) {
   alttap_state.state = 0;
 }
 
-void alt2_finished (tap_dance_state_t *state, void *user_data) {
+ void alt2_finished (tap_dance_state_t *state, void *user_data) {
   alttap_state.state = cur_dance(state);
   switch (alttap_state.state) {
     case SINGLE_TAP: layer_on(_OSR_T_1); is_oneshot_active = true; break;
