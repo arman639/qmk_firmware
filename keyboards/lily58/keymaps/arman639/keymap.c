edad94261aa9a05c33c028c6f37bad77a6e5c7d8
arman639 <arman.paulo.me@gmail.com>
2024-12-24 18:59:07 +0800

    add home row modifiers for the function layer

--- keyboards/lily58/keymaps/arman639/keymap.c
+++ keyboards/lily58/keymaps/arman639/keymap.c
@@ -107,7 +107,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 [_NUMFUNC] = LAYOUT(
   KC_TRNS, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5,                                           KC_NO, KC_NO, KC_NO, KC_NO, KC_NO, KC_NUM,
   KC_LALT, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7,                                           KC_PAST, KC_P7, KC_P8, KC_P9, KC_PPLS, KC_PAUS,
-  KC_LSFT, KC_F2, KC_F10, KC_F11, KC_F12, KC_F8,                                        KC_F9, KC_P4, KC_P5, KC_P6, KC_BSPC, KC_ESC,
+  KC_LSFT, KC_F2, KC_F10, KC_F11, KC_F12, KC_F8,                                        KC_F9, ALT_T(KC_P4), SFT_T(KC_P5), CTL_T(KC_P6), KC_BSPC, KC_ESC,
   KC_LCTL, KC_F1, LALT(KC_LEFT), LALT(KC_RGHT), LALT(KC_F5), KC_F9, LGUI(KC_PGUP),   KC_TRNS, KC_PMNS, KC_P1, KC_P2, KC_P3, KC_PSLS, KC_RCTL,
                     KC_TRNS, KC_TRNS, KC_NO, KC_TRNS,                                   KC_ENT, KC_P0, KC_PDOT, KC_RALT),
 
